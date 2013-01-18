139fdab2070ef627ad8335647b44028b44fcc35e
bolid <dmitriy.grigorev@gmail.com>
2013-01-19 01:40:25 +0400

    Example

--- Hello World/src/com/example/Hello_World/MyActivity.java
+++ Hello World/src/com/example/Hello_World/MyActivity.java
@@ -2,14 +2,26 @@ package com.example.Hello_World;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import  android.widget.Button;
+import  android.widget.TextView;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    TextView textView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Круть!!!");
+            }
+        });
     }
 }
