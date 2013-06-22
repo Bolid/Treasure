14fc6edd2840428404c1f7d314311bcddf506b70
Bolid <dmitriy.grigorev@gmail.com>
2013-06-22 15:24:45 +0400

    Вывод платежей по граику/Вывод с переплатой

--- CreditCalc/src/com/CC/CreditCalc/Arithmetic.java
+++ CreditCalc/src/com/CC/CreditCalc/Arithmetic.java
@@ -5,22 +5,43 @@ import android.util.Log;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arithmetic {
     public static final String TAG = "com.CC.CreditCalc.Arithmetic";
-    Double sumCredit = 0.0;
     int termCredit = 0;
+    Double sumCredit = 0.0;
     Double percend = 0.0;
+    Double dopPlatej = 0.0;
+    List <String> allResult;
 
     public Arithmetic(Double sumCredit, Integer termCredit, Double percend){
         this.sumCredit = sumCredit;
         this.termCredit = termCredit;
         this.percend = Double.valueOf(percend);
-        getTerm();
+        allResult = new ArrayList<String>();
+        allResult.add(0, String.valueOf(getPlatej()));
+        allResult.add(1, String.valueOf(getDelta(getPlatej())));
+        Log.d(TAG, "List_0: " + allResult.get(0));
+        Log.d(TAG, "List_1: " + allResult.get(1));
 //        Log.d(TAG, "Срок: " + getTest());
     }
 
-    public Double setPlatej(){
+    public Arithmetic(Double sumCredit, Integer termCredit, Double percend, Double dopPlatej){
+        this.sumCredit = sumCredit;
+        this.termCredit = termCredit;
+        this.percend = percend;
+        this.dopPlatej = dopPlatej;
+        allResult = new ArrayList<String>();
+        allResult.add(0, String.valueOf(getPlatej()));
+        allResult.add(1, String.valueOf(getDelta(getPlatej())));
+        getTerm();
+    }
+
+
+
+    private Double getPlatej(){
         BigDecimal platej = BigDecimal.valueOf(sumCredit * ((percend/100./12) * Math.pow((1+(percend/100./12)), termCredit)) / (Math.pow((1+(percend/100./12)),termCredit) - 1));
         platej = (platej.setScale(2, BigDecimal.ROUND_HALF_DOWN));
         Double result = Double.valueOf(platej.toString());
@@ -28,22 +49,20 @@ public class Arithmetic {
         return result;
     }
 
-    public Double setDelta(Double platej){
+    private Double getDelta(Double platej){
         BigDecimal delta = BigDecimal.valueOf(Double.valueOf(platej.toString()) * termCredit - sumCredit);
         delta = delta.setScale(2, BigDecimal.ROUND_HALF_DOWN);
         Double result = Double.valueOf(delta.toString());
-        Log.d(TAG, ": " + result);
+        //Log.d(TAG, ": " + result);
         return result;
     }
 
-    public void getTerm(){
+    private void getTerm(){
         int i = 0;
-        Double constPlat = setPlatej();
+        Double constPlat = Double.valueOf(allResult.get(0));
+        Double deltaMounth = sumCredit * (percend/100.) / 12;;
+        Double deltaPlatej = constPlat - deltaMounth;
         Double allPer = 0.0;
-        Double deltaPlatej;
-        Double deltaMounth;
-        deltaMounth = sumCredit * (percend/100.) / 12;
-        deltaPlatej = constPlat - deltaMounth;
         while (sumCredit > 0){
             deltaMounth = sumCredit * (percend/100.) / 12;
             if (sumCredit < deltaPlatej){
@@ -53,7 +72,7 @@ public class Arithmetic {
             else{
                 //deltaPlatej = (1 + percend/100./12) * deltaPlatej;
                 deltaPlatej = constPlat - deltaMounth;
-                sumCredit = sumCredit - deltaPlatej - 20000;
+                sumCredit = sumCredit - deltaPlatej - dopPlatej;
             }
             i++;
             termCredit--;
@@ -71,14 +90,19 @@ public class Arithmetic {
             Log.d(TAG, "Общая переплата: " + allPer);
             Log.d(TAG, "________");
         }
+        BigDecimal roundValue = BigDecimal.valueOf(allPer);
+        roundValue = roundValue.setScale(2, BigDecimal.ROUND_HALF_DOWN);
+        allPer = Double.valueOf(roundValue.toString());
+        allResult.add(2, String.valueOf(allPer));
+        allResult.add(3, String.valueOf(i));
         //Log.d(TAG, "Срок: " + i);
         //Log.d(TAG, "Общая переплата: " + allPer);
         //return newTerm;
     }
 
-    int  getTest(){
+    /*int  getTest(){
         BigDecimal result = BigDecimal.valueOf(((Math.log(setPlatej() / (setPlatej() - (percend/100./12) * sumCredit))) / (Math.log(1 + (percend/100./12)))));
         result = result.setScale(0, BigDecimal.ROUND_HALF_DOWN);
         return Integer.valueOf(result.toString());
-    }
+    }*/
 }
