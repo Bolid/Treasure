284aa43d34863d185ec9c0b8f714ef2f90b5fa23
Bolid <dmitriy.grigorev@gmail.com>
2013-08-07 23:03:23 +0400

    Тестовый экран по скетчу

---
+++ DebtCalc/src/ru/omdroid/DebtCalc/Forms/TestActivity.java
@@ -0,0 +1,14 @@
+package ru.omdroid.DebtCalc.Forms;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.omdroid.DebtCalc.R;
+
+
+public class TestActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_main);
+    }
+}
